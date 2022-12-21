7c799c8afcd9c6cc15a769c55831b2704bb9f110
shawnteh1711 <34572151+shawnteh1711@users.noreply.github.com>
2022-12-21 03:53:17 +0000

    vector

--- header/vector.hpp
+++ header/vector.hpp
@@ -6,12 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 03:08:21 by codespace         #+#    #+#             */
-/*   Updated: 2022/12/21 03:20:03 by codespace        ###   ########.fr       */
+/*   Updated: 2022/12/21 03:41:17 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+namespace ft 
+{
+    template <typename T>
+    class Vector
+    {
+        
+    };    
+}
 
 #endif
